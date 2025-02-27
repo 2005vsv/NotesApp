0a8b6daec1a7a64fa30ea8f3a710a2837b6bd45b
vaishnav vernekar <vernekarvaishnav05@gmail.com>
2025-02-27 19:30:33 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class="">hello</h1>
+      <p class="text-amber-300">tailwindcss</p>
+    </>
+  )
+}
+
+export default App
